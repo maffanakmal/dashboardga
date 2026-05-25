c5ffca2bedb38aaac29ac04d10e7f0745ff0145a
maffanakmal <maffanakmal@gmail.com>
2026-05-25 14:54:08 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
@@ -11,5 +13,7 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
